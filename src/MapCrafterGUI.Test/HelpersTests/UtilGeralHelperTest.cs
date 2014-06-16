f062b6656998232d6666412516fa25c92debcc19
cabralRodrigo <rodrigopaivac@gmail.com>
2014-06-16 10:00:00 -0300

    Add project for tests

---
+++ src/MapCrafterGUI.Test/HelpersTests/UtilGeralHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using MapCrafterGUI.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MapCrafterGUI.Test.HelpersTests
+{
+    [TestClass]
+    public class UtilHelperTest
+    {
+        [TestMethod]
+        public void StringReplaceWithMetadataTest()
+        {
+            string pattern = "This is a {NameOfObject}. This is a {NameOfAction}. I can replace integers too, look: {IntValue}";
+            object patternFiller = new { NameOfObject = "string", NameOfAction = "test", IntValue = 15 };
+
+            string successresult = "This is a string. This is a test. I can replace integers too, look: 15";
+
+            string result = UtilHelper.StringReplaceWithMetadata(pattern, patternFiller);
+
+            Assert.AreEqual<string>(result, successresult);
+        }
+    }
+}
